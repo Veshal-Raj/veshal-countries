70fceecf57661cbbb80bef8c713a7e70184469c2
Veshal Raj <veshalraj1307@gmail.com>
2024-05-03 12:35:05 +0530

    stage-1

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a simple component for fetching country package
---
+++ rollup.config.js
@@ -0,0 +1,25 @@
+import { terser } from 'rollup-plugin-terser'
+ 
+
+
+export default [
+    {
+        input: 'src/index.js',
+        output: {
+            file: "dist/index.js",
+            format: 'es',
+            plugins: [
+                terser({
+                    ecma: 2020,
+                    mangle: { toplevel: true },
+                    compress: {
+                        module: true,
+                        toplevel: true,
+                        unsafe_arrows: true                       
+                    },
+                    output: { quote_style: 1 }
+                })
+            ]
+        }
+    }
+]
